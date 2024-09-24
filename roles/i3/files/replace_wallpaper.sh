d5c42b735873e42ce5d9f554c1ea93b49bf87e98
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-09-24 17:08:18 +0200

    added lockscreen functionality to i3

--- roles/i3/files/replace_wallpaper.sh
+++ roles/i3/files/replace_wallpaper.sh
@@ -1,1 +1,7 @@
 wget -O ~/.config/wallpaper.jpg https://unsplash.it/1920/1080?random
+
+# convert jpg to png for lock screen adjust accordingly to screen resolution
+convert -scale 3840x2160 ~/.config/wallpaper.jpg ~/.config/wallpaper.png
+
+# reload i3
+i3-msg restart
