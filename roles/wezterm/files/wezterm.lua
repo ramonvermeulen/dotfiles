832e424fd077a38b4820fb31c2258ece9daa4457
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-09-26 21:30:24 +0200

    config works well

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- using picom for opacity, leaving this here in case I want to switch
-config.window_background_opacity = 1.0
+-- apply-ing minor opacity
+config.window_background_opacity = 0.95
 
 return config
