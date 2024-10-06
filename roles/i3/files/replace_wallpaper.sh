68623e7c53401286bd18c01921f7c5ad0333fdeb
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-06 13:21:08 +0200

    change wallpaper size + remove redundant tasks

--- roles/i3/files/replace_wallpaper.sh
+++ roles/i3/files/replace_wallpaper.sh
@@ -1,7 +1,7 @@
-wget -O ~/.config/wallpaper.jpg https://unsplash.it/1920/1080?random
+wget -O ~/.config/wallpaper.jpg https://unsplash.it/3840/2400?random
 
 # convert jpg to png for lock screen adjust accordingly to screen resolution
-convert -scale 3840x2160 ~/.config/wallpaper.jpg ~/.config/wallpaper.png
+convert -scale 3840x2400 ~/.config/wallpaper.jpg ~/.config/wallpaper.png
 
 # reload i3
 i3-msg restart
