332bd262395351818ad759b2db2c08e3c196ad45
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-06 11:46:27 +0200

    completions for nvim

--- roles/neovim/files/nvim/lua/plugins/lsp-config.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp-config.lua
@@ -1,3 +1,6 @@
+-- Define the list of LSP servers to avoid repetition
+local servers = { "lua_ls", "pyright" }
+
 return {
     {
         "williamboman/mason.nvim",
@@ -9,7 +12,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = servers -- Reuse the list of servers here
             })
         end
     },
@@ -17,9 +20,18 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            -- Use a loop to set up LSP servers, avoiding duplication
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    capabilities = capabilities
+                })
+            end
+
+            -- Keymap for code actions
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
+
--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -3,6 +3,6 @@ local config = {}
 
 -- using picom for opacity, leaving this here in case I want to switch
 -- apply-ing minor opacity
-config.window_background_opacity = 0.95
+config.window_background_opacity = 1.0
 
 return config
