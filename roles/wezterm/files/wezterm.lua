376378a61986261cf40f5a158c8509dd6b26da11
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-10-08 14:34:00 +0200

    Update wezterm.lua

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -1,8 +1,158 @@
-local wezterm = require 'wezterm'
-local config = {}
+local wezterm = require("wezterm")
 
--- using picom for opacity, leaving this here in case I want to switch
--- apply-ing minor opacity
-config.window_background_opacity = 1.0
+-- kanagawa: https://github.com/rebelot/kanagawa.nvim/blob/master/extras/wezterm.lua
+local config = {
+        force_reverse_video_cursor = true,
+        colors = {
+                foreground = "#dcd7ba",
+                background = "#1f1f28",
+
+                cursor_bg = "#c8c093",
+                cursor_fg = "#c8c093",
+                cursor_border = "#c8c093",
+
+                selection_fg = "#c8c093",
+                selection_bg = "#2d4f67",
+
+                scrollbar_thumb = "#16161d",
+                split = "#16161d",
+
+                ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+                brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+                indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+        },
+}
+
+config.tab_bar_style = {
+  active_tab_left = wezterm.format {
+    { Background = { Color = '#FFFFFF' } },
+    { Foreground = { Color = '#2b2042' } },
+  },
+  active_tab_right = wezterm.format {
+    { Background = { Color = '#FFFFFF' } },
+    { Foreground = { Color = '#2b2042' } },
+  },
+  inactive_tab_left = wezterm.format {
+    { Background = { Color = '#FFFFFF' } },
+    { Foreground = { Color = '#1b1032' } },
+  },
+  inactive_tab_right = wezterm.format {
+    { Background = { Color = '#FFFFFF' } },
+    { Foreground = { Color = '#1b1032' } },
+  },
+}
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- tmux
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+    {
+        mods = "LEADER",
+        key = "c",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        mods = "LEADER",
+        key = "x",
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        mods = "LEADER",
+        key = "b",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "\\",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "-",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        mods = "LEADER",
+        key = "LeftArrow",
+        action = wezterm.action.AdjustPaneSize { "Left", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "RightArrow",
+        action = wezterm.action.AdjustPaneSize { "Right", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "DownArrow",
+        action = wezterm.action.AdjustPaneSize { "Down", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "UpArrow",
+        action = wezterm.action.AdjustPaneSize { "Up", 5 }
+    },
+}
+
+for i = 0, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i),
+    })
+end
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+    local SOLID_LEFT_ARROW = ""
+    local ARROW_FOREGROUND = { Foreground = { Color = "#1f1f28" } }
+    local prefix = ""
+
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
+
+    if window:active_tab():tab_id() ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1f1f28" } }
+    end -- arrow color based on if tab is first pane
+
+    window:set_left_status(wezterm.format {
+        { Background = { Color = "#1f1f28" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW }
+    })
+end)
 
 return config
