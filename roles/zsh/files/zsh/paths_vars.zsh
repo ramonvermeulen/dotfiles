013cec263224bf0af95ce71de1ee80ea68acbd74
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-08 21:07:49 +0200

    feat: add rust role

--- roles/zsh/files/zsh/paths_vars.zsh
+++ roles/zsh/files/zsh/paths_vars.zsh
@@ -3,5 +3,6 @@
 addToPath $HOME/.local/bin # pip user level installs
 addToPath $HOME/.dotfiles/bin
 addToPath /usr/local/go/bin
+addToPath $HOME/.cargo/bin
 addToPath /opt/nvim-linux64/bin
 addToPath $PYENV_ROOT/bin
