a978f742db8bf8296f35ad5866636e3f2e781094
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-09 12:56:33 +0200

    feat: split up .zshrc + bug in nvm role

---
+++ roles/zsh/files/zsh/nvm_setup.zsh
@@ -0,0 +1,22 @@
+# slow zsh start-up time, so made nvm lazy load
+lazy_load_nvm() {
+  unset -f npm node nvm
+  export NVM_DIR=~/.nvm
+  [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+}
+
+npm() {
+  lazy_load_nvm
+  npm $@
+}
+
+node() {
+  lazy_load_nvm
+  node $@
+}
+
+nvm() {
+  lazy_load_nvm
+  nvm $@
+}
