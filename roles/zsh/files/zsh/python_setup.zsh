64faf4d66e8188a4625813fd19493daabe65b44b
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-09 13:06:33 +0200

    feat: added function to automatically active venv

--- roles/zsh/files/zsh/python_setup.zsh
+++ roles/zsh/files/zsh/python_setup.zsh
@@ -1,2 +1,7 @@
 eval "$(pyenv init -)"
 
+act() {
+    [ -f "bin/activate" ] && source bin/activate
+    [ -f ".venv/bin/activate" ] && source .venv/bin/activate
+    [ -f "venv/bin/activate" ] && source .venv/bin/activate
+}
